745b94e2a422c6647c61d8cbc8a0de1a1d2bf250
nelsoncaquilala <30279226+nelsoncaquilala@users.noreply.github.com>
2022-05-23 16:39:08 +0800

    Update inventoryo.php

--- www/ajax/inventoryo.php
+++ www/ajax/inventoryo.php
@@ -13,45 +13,74 @@ $username=isset($_POST['username']) ? $_POST['username']: '';
 $today = date("Y-m-d");
 
 
-$mysq=mysqli_query($con,"SELECT * from inventory_in where item_id='$id' and quantity>0 
-order by  date_received desc");
+// $mysq=mysqli_query($con,"SELECT * from inventory_in where item_id='$id' and quantity>0 
+// order by  id desc");
+
+$mysq=mysqli_query($con,"SELECT * FROM inventory_in 
+  WHERE item_id='$id' and quantity>0 
+  ORDER BY id asc LIMIT 1");
 $myd=mysqli_fetch_array($mysq);
 $price=$myd['unit_price'];
 $quantity=$myd['quantity'];
-$date_received=$myd['date_received'];
+$firstid=$myd['id'];
 
-if($quantity < $qty)
+$mysqqq=mysqli_query($con,"SELECT sum(quantity)as quantity FROM inventory_in 
+  WHERE item_id='$id' and quantity>0");
+$mydq=mysqli_fetch_array($mysqqq);
+$quantityy=$mydq['quantity'];
+// $date_received=$myd['date_received'];
+
+if($quantityy < $qty)
 {
     //insuficient stocks
     echo json_encode(1);
 }
 else
 {
-    $my=mysqli_query($con,
-    "INSERT INTO inventory_out
-    (
-        item_id,
-        quantity,
-        unit_price,
-        date_issued,
-        issued_by
-    )
-    VALUES
-    (
-    '$id',
-    '$qty',
-    '$price',
-    '$today',
-    '$username'
-    )
-    ");
+        $my=mysqli_query($con,
+        "INSERT INTO inventory_out
+        (
+            item_id,
+            quantity,
+            unit_price,
+            date_issued,
+            issued_by
+        )
+        VALUES
+        (
+        '$id',
+        '$qty',
+        '$price',
+        '$today',
+        '$username'
+        )
+        ");
     
 
-    $sql="UPDATE inventory_in set quantity=quantity-$qty 
-    Where id='$id' and date_received='$date_received'";
-    $res=mysqli_query($con,$sql);
+        $sql="UPDATE inventory_in set quantity=quantity-$qty 
+        Where item_id='$id' and id='$firstid'";
+        $res=mysqli_query($con,$sql);
 
-    echo json_encode(2);
+        $mysq11=mysqli_query($con,"SELECT * FROM inventory_in 
+        WHERE item_id='$id' and quantity<0 
+        ORDER BY id asc LIMIT 1");
+        $myd11=mysqli_fetch_array($mysq11);
+        $quantity11=$myd11['quantity'];
+        // $netqty=$quantity($quantity11);
+
+
+        //for minus the asccending value
+        $sq="UPDATE inventory_in set quantity=$quantity11+quantity
+        WHERE item_id='$id' and quantity>0 
+        ORDER BY id asc LIMIT 1";
+        $re=mysqli_query($con,$sq);
+
+        //for minus the negative value
+        $sqll="UPDATE inventory_in set quantity=0
+        Where item_id='$id' and quantity<0  ";
+        $ress=mysqli_query($con,$sqll);
+
+        echo json_encode(2);
 }
     
 
